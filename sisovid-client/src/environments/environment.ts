02835baf5f2572d624a8a9fc96aa58f102e85c1f
Deathmetall <40876487+tomasmetal23@users.noreply.github.com>
2020-10-26 16:09:38 -0400

    Update and rename environment.prod.ts to environment.ts

--- sisovid-client/src/environments/environment.ts
+++ sisovid-client/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     hmr       : false,
-    api: 'http://192.168.253.100/api/',
+    api: 'http://sisovid_api/',
     //api: 'http://192.168.112.2/api/',
     base_href: '/'
 };
