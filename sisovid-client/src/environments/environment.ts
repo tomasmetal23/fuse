f62929537599b8bd48d1aba04d9a5e7cd1404a34
Deathmetall <40876487+tomasmetal23@users.noreply.github.com>
2020-10-31 15:44:43 -0400

    Update environment.ts

--- sisovid-client/src/environments/environment.ts
+++ sisovid-client/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     hmr       : false,
-    api: 'http://sisovid_api/',
+    api: 'http://66.42.86.22:8010',
     //api: 'http://192.168.112.2/api/',
     base_href: '/'
 };
